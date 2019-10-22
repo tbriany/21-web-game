c264443d27450b20eedd4ab86bfe4823754ddfc5
tbriany <brianytaveras@pursuit.org>
2019-10-22 16:35:43 -0400

    created function that draws cards

--- simple_21.js
+++ simple_21.js
@@ -11,4 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
     let response = await axios.get(url)
     deck_id = response.data.deck_id 
+    drawCards(deck_id, 2)
 }
+
+const drawCards = async (deck_id, num) => {
+    let url = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=${num}`
+    let response = await axios.get(url)
+    let cardData = response.data.cards 
+  }
